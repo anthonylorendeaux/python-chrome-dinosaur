704ee4e2a4739edf7eeb2bd7e781d2a09fdb00b8
Anthony Lorendeaux <anthony.lorendeaux@free.fr>
2020-12-28 23:49:47 +0100

    Add Comments
    - Comments main.py
    - Comments  Dinosaur.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ SCREEN_HEIGHT = 600
 SCREEN_WIDTH = 1100
 SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# Images Loading
+# TODO - To Remove
 SMALL_CACTUS = [pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus1.png")),
                 pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus2.png")),
                 pygame.image.load(os.path.join("Assets/Cactus", "SmallCactus3.png"))]
@@ -25,11 +25,15 @@ CLOUD = pygame.image.load(os.path.join("Assets/Other", "Cloud.png"))
 
 BG = pygame.image.load(os.path.join("Assets/Other", "Track.png"))
 
+# * Main function
 def main():
     run = True
     clock = pygame.time.Clock()
+
+    # Instanciate Class
     player = Dinosaur()
 
+    # Game Loop
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
