0831c960916f1bc115f5da6e03b5ac3278333a98
Anthony Lorendeaux <anthony.lorendeaux@free.fr>
2020-12-29 11:26:01 +0100

    Create Cloud.py file
    - Move Cloud class into Cloud.py file

---
+++ Cloud.py
@@ -0,0 +1,22 @@
+import pygame
+import os
+import random
+
+# Images Loading
+CLOUD = pygame.image.load(os.path.join("Assets/Other", "Cloud.png"))
+
+class Cloud:
+    def __init__(self, SCREEN_WIDTH):
+        self.x = SCREEN_WIDTH + random.randint(800, 1000)
+        self.y = random.randint(50, 100)
+        self.image = CLOUD
+        self.width = self.image.get_width()
+
+    def update(self, SCREEN_WIDTH, game_speed):
+        self.x -= game_speed
+        if self.x < -self.width:
+            self.x = SCREEN_WIDTH + random.randint(2500, 3000)
+            self.y = random.randint(50, 100)
+
+    def draw(self, SCREEN):
+        SCREEN.blit(self.image, (self.x, self.y))
